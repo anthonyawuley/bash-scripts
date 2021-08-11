ea3bf01b8196a13c21a98c33e71c774e9c36806d
Anthony Awuley <dev@awuley.com>
2021-08-10 23:54:35 -0400

    Bug fix for random_number
    Bug fix for random_number thats causing sed to throw an exception

--- random-word-generator.sh
+++ random-word-generator.sh
@@ -23,7 +23,7 @@ non_random_words=`cat $ALL_NON_RANDOM_WORDS | wc -l`
 while [ "$X" -lt "$1" ]
 do
 random_number=`od -N3 -An -i /dev/urandom |
-awk -v f=0 -v r="$non_random_words" '{printf "%i\n", f + r * $1 / 16777216}'`
+awk -v f=0 -v r="$non_random_words" '{printf "%i\n", f + r * $1 / 16777216}' | head -1`
 sed `echo $random_number`"q;d" $ALL_NON_RANDOM_WORDS
   let "X = X + 1"
 done
